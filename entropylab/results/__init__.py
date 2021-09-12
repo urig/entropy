d5d64bb3a985a31304c831c4b0d92749813b0b87
Uri Goldstein <uri.goldstein@gmail.com>
2021-09-12 10:36:51 +0300

    Letting user choose --port in CLI entropy serve

--- entropylab/results/__init__.py
+++ entropylab/results/__init__.py
@@ -3,13 +3,19 @@ import sys
 
 import panel as pn
 
+from entropylab.results_backend.sqlalchemy.project import project_name
+
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("../../.."))
 
 from entropylab.results.dashboard.panel_dashboard import Dashboard  # noqa: E402
 
 
-def serve_results(path: str):
+def serve_results(path: str, port: int = 0):
     pn.extension()
     dashboard = Dashboard(path)
-    pn.serve(dashboard.servable())
+    pn.serve(
+        dashboard.servable(),
+        port=port,
+        title=f"'{project_name(path)}' - Entropy experiment results",
+    )
